2337bddd08fe8a5abe176f3f17e6c38fc763ad33
joshoti <joshoti539@gmail.com>
2024-10-10 03:58:25 -0700

    Made header use sticky position

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,8 +1,15 @@
 import { IconLogin2 } from "@tabler/icons-react";
 import { Group, Box, Text, Button, Container } from "@mantine/core";
 import classes from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const navigateToRegister = () => {
+    navigate("/register");
+  };
+
   return (
     <Box className={classes.container}>
       <Container maw={"100%"} ml={20} mr={20} h={70}>
@@ -16,11 +23,12 @@ export const Header = () => {
             <Text className={`${classes.text}`}>Support</Text>
             <Button
               radius={"md"}
+              onClick={navigateToRegister}
               variant="gradient"
               gradient={{ from: "indigo", to: "violet", deg: 90 }}
               leftSection={<IconLogin2 size={20} />}
             >
-              <Text className={`${classes.text}`}>Get Started</Text>
+              <Text className={`${classes.getStarted}`}>Get Started</Text>
             </Button>
           </Group>
         </Group>
