49a49d866e5fa8d39d847a2e0c2f8ebeaf55ea5d
joshoti <joshoti539@gmail.com>
2024-10-10 03:58:42 -0700

    removed the extra div around the footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -7,7 +7,6 @@ import {
 } from "@tabler/icons-react";
 import {
   Group,
-  Box,
   Text,
   Container,
   Tabs,
@@ -24,95 +23,97 @@ export const Footer = () => {
   const iconStylePanel = { width: rem(20), height: rem(20) };
 
   return (
-    <Box bg={"var(--mantine-color-dark-9)"}>
-      <Container h={150} className={classes.header}>
-        <Group>
-          <Group gap={4}>
-            <IconCopyright size={18} color="gray" />
-            <Text
-              fw={400}
-              size="lg"
-              style={{ color: "gray", margin: 0, padding: 0 }}
-            >
-              2024
-            </Text>
-            <Text className={`${classes.title}`}>DevPortalX</Text>
-            <IconDeviceLaptop size={25} color="white" />
-          </Group>
-
-          <Group className={`${classes.headerRight}`}>
-            <Tabs defaultValue="connect">
-              <Tabs.List>
-                <Tabs.Tab
-                  value="connect"
-                  className={classes.tabs}
-                  leftSection={
-                    <IconBrandLinkedin color="white" style={iconStyle} />
-                  }
-                >
-                  <Text className={classes.text}>Connect</Text>
-                </Tabs.Tab>
-                <Tabs.Tab
-                  value="with"
-                  className={classes.tabs}
-                  leftSection={
-                    <IconBrandGithubFilled color="white" style={iconStyle} />
-                  }
-                >
-                  <Text className={classes.text}>With</Text>
-                </Tabs.Tab>
-                <Tabs.Tab
-                  value="me"
-                  className={classes.tabs}
-                  leftSection={
-                    <IconBrandInstagram color="white" style={iconStyle} />
-                  }
-                >
-                  <Text className={classes.text}>Me</Text>
-                </Tabs.Tab>
-              </Tabs.List>
-              <Tabs.Panel value="connect">
-                <Anchor
-                  href="https://www.linkedin.com/in/joshua-oti/"
-                  target="_blank"
-                >
-                  <Button
-                    variant="transparent"
-                    leftSection={<IconBrandLinkedin style={iconStylePanel} />}
-                  >
-                    <Text className={`${classes.text}`}>joshua-oti</Text>
-                  </Button>
-                </Anchor>
-              </Tabs.Panel>
-              <Tabs.Panel value="with">
-                <Anchor href="https://github.com/joshoti" target="_blank">
-                  <Button
-                    variant="transparent"
-                    leftSection={
-                      <IconBrandGithubFilled
-                        color="white"
-                        style={iconStylePanel}
-                      />
-                    }
-                  >
-                    <Text className={classes.text}>joshoti</Text>
-                  </Button>
-                </Anchor>
-              </Tabs.Panel>
-              <Tabs.Panel value="me">
-                <Anchor href="https://instagram.com/josh_ohtee" target="_blank">
-                  <Button
-                    variant="transparent"
-                    leftSection={<Image src={IgLogo} h={23} />}
-                  >
-                    <Text className={classes.text}>josh_ohtee</Text>
-                  </Button>
-                </Anchor>
-              </Tabs.Panel>
-            </Tabs>
-          </Group>
+    <Container
+      bg={"var(--mantine-color-dark-9)"}
+      h={150}
+      className={classes.header}
+    >
+      <Group>
+        <Group gap={4}>
+          <IconCopyright size={18} color="gray" />
+          <Text
+            fw={400}
+            size="lg"
+            style={{ color: "gray", margin: 0, padding: 0 }}
+          >
+            2024
+          </Text>
+          <Text className={`${classes.title}`}>DevPortalX</Text>
+          <IconDeviceLaptop size={25} color="white" />
         </Group>
-      </Container>
-    </Box>
+
+        <Group className={`${classes.headerRight}`}>
+          <Tabs defaultValue="connect">
+            <Tabs.List>
+              <Tabs.Tab
+                value="connect"
+                className={classes.tabs}
+                leftSection={
+                  <IconBrandLinkedin color="white" style={iconStyle} />
+                }
+              >
+                <Text className={classes.text}>Connect</Text>
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="with"
+                className={classes.tabs}
+                leftSection={
+                  <IconBrandGithubFilled color="white" style={iconStyle} />
+                }
+              >
+                <Text className={classes.text}>With</Text>
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="me"
+                className={classes.tabs}
+                leftSection={
+                  <IconBrandInstagram color="white" style={iconStyle} />
+                }
+              >
+                <Text className={classes.text}>Me</Text>
+              </Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel value="connect">
+              <Anchor
+                href="https://www.linkedin.com/in/joshua-oti/"
+                target="_blank"
+              >
+                <Button
+                  variant="transparent"
+                  leftSection={<IconBrandLinkedin style={iconStylePanel} />}
+                >
+                  <Text className={`${classes.text}`}>joshua-oti</Text>
+                </Button>
+              </Anchor>
+            </Tabs.Panel>
+            <Tabs.Panel value="with">
+              <Anchor href="https://github.com/joshoti" target="_blank">
+                <Button
+                  variant="transparent"
+                  leftSection={
+                    <IconBrandGithubFilled
+                      color="white"
+                      style={iconStylePanel}
+                    />
+                  }
+                >
+                  <Text className={classes.text}>joshoti</Text>
+                </Button>
+              </Anchor>
+            </Tabs.Panel>
+            <Tabs.Panel value="me">
+              <Anchor href="https://instagram.com/josh_ohtee" target="_blank">
+                <Button
+                  variant="transparent"
+                  leftSection={<Image src={IgLogo} h={23} />}
+                >
+                  <Text className={classes.text}>josh_ohtee</Text>
+                </Button>
+              </Anchor>
+            </Tabs.Panel>
+          </Tabs>
+        </Group>
+      </Group>
+    </Container>
   );
 };
