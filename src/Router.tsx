289538e1c13d12f2c9875bd0939d66e8e15eda27
joshoti <joshoti539@gmail.com>
2024-10-10 03:58:11 -0700

    Added register/login to the router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./app/pages/Home.page";
+import { Register } from "./app/pages/Register.page";
+import { Login } from "./app/pages/Login.page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
