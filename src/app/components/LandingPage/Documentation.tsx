cb0d97f068ea34aea319850cc23e063593431e9a
joshoti <joshoti539@gmail.com>
2024-10-11 04:17:08 -0700

    Added explanation for documentation section

--- src/app/components/LandingPage/Documentation.tsx
+++ src/app/components/LandingPage/Documentation.tsx
@@ -18,6 +18,7 @@ export function Documentation() {
     shadow: "sm",
     padding: "lg",
   };
+  const cardSectionHeight = 200;
   const cardRadius = 20;
   const cardImageMargin = 10;
   const scrollBehavior: Object = {
@@ -48,7 +49,9 @@ export function Documentation() {
             <Card.Section>
               <Box m={cardImageMargin}>
                 <Image
-                  src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                  h={cardSectionHeight}
+                  src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
+                  // src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
                   radius={cardRadius - cardImageMargin}
                 />
               </Box>
@@ -57,9 +60,9 @@ export function Documentation() {
               <Text className={classes.docsTitle}>OAuth 2.0 API</Text>
             </Group>
             <Text c="dimmed" className={classes.singleDocsText}>
-              With Fjord Tours you can explore more of the magical fjord
-              landscapes with tours and activities on and around the fjords of
-              Norway
+              OAuth 2.0 allows secure access to user data without sharing
+              credentials by using tokens, enabling streamlined authentication
+              across multiple platforms.
             </Text>
             <Button color="#5345c8" fullWidth mt="md" radius="md">
               Explore API
@@ -78,7 +81,8 @@ export function Documentation() {
             <Card.Section>
               <Box m={cardImageMargin}>
                 <Image
-                  src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                  h={cardSectionHeight}
+                  src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
                   radius={cardRadius - cardImageMargin}
                 />
               </Box>
@@ -88,9 +92,9 @@ export function Documentation() {
               <Badge color="pink">New!</Badge>
             </Group>
             <Text c="dimmed" className={classes.singleDocsText}>
-              With Fjord Tours you can explore more of the magical fjord
-              landscapes with tours and activities on and around the fjords of
-              Norway
+              Webhooks send real-time data to external systems, automating
+              processes and enabling efficient communication between
+              applications when specific events occur.
             </Text>
             <Button color="#5345c8" fullWidth mt="md" radius="md">
               Learn More
@@ -109,7 +113,8 @@ export function Documentation() {
             <Card.Section>
               <Box m={cardImageMargin}>
                 <Image
-                  src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                  h={cardSectionHeight}
+                  src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
                   radius={cardRadius - cardImageMargin}
                 />
               </Box>
@@ -119,9 +124,9 @@ export function Documentation() {
               <Badge color="blue">Stay Tuned!</Badge>
             </Group>
             <Text c="dimmed" className={classes.singleDocsText}>
-              With Fjord Tours you can explore more of the magical fjord
-              landscapes with tours and activities on and around the fjords of
-              Norway
+              The real-time analytics API delivers instant data insights,
+              helping track performance metrics and user activity for immediate
+              decision-making.
             </Text>
             <Button
               onClick={scrollToUpdates}
