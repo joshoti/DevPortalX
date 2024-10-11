f31048c45fca4975accdd994d3ce3c97beee4df5
joshoti <joshoti539@gmail.com>
2024-10-10 19:31:00 -0700

    Removed the default welcome page

--- src/app/pages/Home.page.tsx
+++ src/app/pages/Home.page.tsx
@@ -4,7 +4,6 @@ import { Hero } from "../components/TypingHero/Hero";
 import { Documentation } from "../components/LandingPage/Documentation";
 import { Updates } from "../components/LandingPage/Updates";
 import { UseCase } from "../components/LandingPage/UseCase";
-import { Welcome } from "../components/LandingPage/Welcome";
 
 export function HomePage() {
   return (
