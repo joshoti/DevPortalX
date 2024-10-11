d4cf47155066523a022e493f971f807b8fdc14ef
joshoti <joshoti539@gmail.com>
2024-10-11 04:20:42 -0700

    Notification system currently renders under the footer. Trying to fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { Router } from "./Router";
 import { theme } from "./theme";
+import { Notifications } from "@mantine/notifications";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <Notifications />
       <Router />
     </MantineProvider>
   );
--- src/app/components/Login/LoginForm.tsx
+++ src/app/components/Login/LoginForm.tsx
@@ -1,36 +1,144 @@
-import { Box, Button, Group, TextInput } from "@mantine/core";
-import { LoginDecoration } from "./LoginDecoration";
+import {
+  Alert,
+  Button,
+  Group,
+  TextInput,
+  PasswordInput,
+  Image,
+  Text,
+  Flex,
+  Transition,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import loginArtBird from "../../assets/images/One-Line-bird.jpg";
+import classes from "./Login.module.css";
+import { IconAt, IconLockPassword } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
+import { api } from "../../api/axios-api";
+import { useState } from "react";
+import { showNotification, notifications } from "@mantine/notifications";
 
 export function LoginForm() {
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
       email: "",
-      termsOfService: false,
-    },
-
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: "",
     },
   });
 
-  return (
-    <Group>
-      <LoginDecoration />
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="your@email.com"
-          key={form.key("email")}
-          {...form.getInputProps("email")}
-        />
+  const textInputStyle = { width: 400 };
+  const inputFieldIconStyle = { size: 18, stroke: 1.5 };
+  type LoginT = {
+    email: string;
+    password: string;
+  };
 
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Log in</Button>
-        </Group>
-      </form>
+  const handleSubmit = (form: LoginT) => {
+    api
+      .post("/api/users/login", { email: form.email, password: form.password })
+      .then(({ data }) => {
+        console.log("Then section");
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("Catch section");
+        showNotification({
+          title: "Error",
+          message: error.response.data.message || "Unable to login",
+          color: "red",
+        });
+        notifications.show({
+          title: "Error",
+          message: error.response.data.message || "Unable to login",
+          color: "red",
+        });
+        console.error({
+          title: error.response.data.message || "Unable to login",
+        });
+      });
+  };
+
+  return (
+    <Group gap={0} h={700}>
+      <Group
+        h={"100%"}
+        flex={0.75}
+        className={classes.loginDecorationContainer}
+      >
+        <Image fit="contain" src={loginArtBird} width={20} />
+      </Group>
+      <Flex align={"center"} justify={"center"} h={"100%"} flex={1}>
+        <Flex direction="column">
+          <Text className={classes.title}>Log In</Text>
+          <Text mb={30} c={"dimmed"} className={classes.subtitle}>
+            Log in to your DevPortalX account
+          </Text>
+          <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+            <TextInput
+              leftSection={
+                <IconAt
+                  stroke={inputFieldIconStyle.stroke}
+                  size={inputFieldIconStyle.size}
+                />
+              }
+              style={textInputStyle}
+              withAsterisk
+              mb={15}
+              size="md"
+              label="Email"
+              placeholder="your@email.com"
+              key={form.key("email")}
+              {...form.getInputProps("email")}
+            />
+            <PasswordInput
+              leftSection={
+                <IconLockPassword
+                  stroke={inputFieldIconStyle.stroke}
+                  size={inputFieldIconStyle.size}
+                />
+              }
+              style={textInputStyle}
+              withAsterisk
+              mb={15}
+              size="md"
+              label="Password"
+              placeholder="password"
+              key={form.key("password")}
+              {...form.getInputProps("password")}
+            />
+
+            <Group justify="end">
+              <Link className={classes.textLink} to="#">
+                <Text c={"dimmed"} className={classes.textLink}>
+                  Forgot Password?
+                </Text>
+              </Link>
+            </Group>
+            <Group mt="xs" mb="md">
+              <Button color="#5345c8" type="submit">
+                Log in
+              </Button>
+            </Group>
+          </form>
+          <Text span mb={5} c={"dimmed"} className={classes.text}>
+            Don't have an account?{" "}
+            <Link className={classes.textLink} to="/register">
+              <Text span className={classes.textLink}>
+                Sign up
+              </Text>
+            </Link>
+          </Text>
+          <Text span c={"dimmed"} className={classes.text}>
+            Can't login?{" "}
+            <Link className={classes.textLink} to="#">
+              <Text span className={classes.textLink}>
+                Get support
+              </Text>
+            </Link>
+          </Text>
+        </Flex>
+      </Flex>
     </Group>
   );
 }
