f63d09d51ad0d664b9b99f13f9d19dc0fb3c2895
joshoti <joshoti539@gmail.com>
2024-10-12 04:34:13 -0700

    Removed the mantine/notifications because it message renders under the window

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { Router } from "./Router";
 import { theme } from "./theme";
-import { Notifications } from "@mantine/notifications";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Notifications />
       <Router />
     </MantineProvider>
   );
