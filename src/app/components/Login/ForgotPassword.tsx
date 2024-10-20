8003f5ad064a896e682dd29236b3779529ac71cd
joshoti <joshoti539@gmail.com>
2024-10-20 16:27:58 -0700

    Completed Forgot password modal

--- src/app/components/Login/ForgotPassword.tsx
+++ src/app/components/Login/ForgotPassword.tsx
@@ -7,24 +7,27 @@ import {
   Flex,
   ThemeIcon,
   PinInput,
+  Transition,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import classes from "./Login.module.css";
-import { IconAt, IconAlertTriangle } from "@tabler/icons-react";
+import { IconAt, IconAlertTriangle, IconCheck } from "@tabler/icons-react";
 import { useState } from "react";
 import { api } from "../../api/axios-api";
 
 export function ForgotPassword() {
+  const [displaySuccess, setDisplaySuccess] = useState(false);
   const [displayAlert, setDisplayAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [disableSubmit, setDisableSubmit] = useState(true);
+  const [displayForm, setDisplayForm] = useState(true);
   const [displayPinInput, setDisplayPinInput] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
   const form = useForm({
     mode: "uncontrolled",
     initialValues: {
       email: "",
-      phone: "",
     },
 
     validate: {
@@ -33,7 +36,6 @@ export function ForgotPassword() {
   });
   type ResetPasswordT = {
     email: string;
-    phone: string;
   };
   const handleSubmit = (form: ResetPasswordT) => {
     api
@@ -41,8 +43,9 @@ export function ForgotPassword() {
         email: form.email,
       })
       .then(({ data }) => {
-        console.log(data);
+        setDisplayForm(false);
         setDisplayPinInput(true);
+        setUserEmail(form.email);
       })
       .catch((error) => {
         const message =
@@ -53,25 +56,16 @@ export function ForgotPassword() {
       });
   };
 
-  const pinForm = useForm({
-    mode: "uncontrolled",
-    initialValues: {
-      email: "",
-      pin: "",
-    },
-  });
-  type PinT = {
-    email: string;
-    pin: string;
-  };
-  const handleSubmitPin = (form: PinT) => {
+  const handleSubmitPin = (form: string) => {
     api
       .post("/api/users/check-otp", {
-        email: form.email,
-        pin: form.pin,
+        email: userEmail,
+        pin: form,
       })
       .then(({ data }) => {
         console.log(data);
+        setDisplayPinInput(false);
+        setDisplaySuccess(true);
       })
       .catch((error) => {
         const message =
@@ -87,11 +81,14 @@ export function ForgotPassword() {
 
   return (
     <Flex align={"center"} justify={"center"} h={"100%"} flex={1}>
-      <Flex direction="column">
+      {/* Forgot Password Form */}
+      <Flex
+        style={{ display: displayForm ? "flex" : "none" }}
+        direction="column"
+      >
         <Text mb={30} c={"dimmed"} className={classes.subtitle}>
           Send OTP to email
         </Text>
-        {/* Forgot Password form */}
         <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
           <TextInput
             leftSection={
@@ -143,30 +140,85 @@ export function ForgotPassword() {
             </Button>
           </Group>
         </form>
-
-        {/* OTP input */}
-        <form onSubmit={pinForm.onSubmit((values) => handleSubmitPin(values))}>
-          <PinInput />
-
-          <Group mt={3} gap={0}>
-            <ThemeIcon
-              className={`${classes.errorMessage} ${classes.caution}`}
-              style={{
-                display: displayAlert ? "flex" : "none",
-              }}
-            >
-              <IconAlertTriangle size={20} />
-            </ThemeIcon>
-            <Text
-              style={{
-                display: displayAlert ? "block" : "none",
-              }}
-              className={classes.errorMessage}
-              children={alertMessage}
-            ></Text>
-          </Group>
-        </form>
       </Flex>
+
+      {/* OTP input */}
+      <Flex
+        style={{ display: displayPinInput ? "flex" : "none" }}
+        direction="column"
+      >
+        <Transition
+          mounted={displayPinInput}
+          transition={"fade-right"}
+          duration={200}
+          timingFunction="ease"
+          keepMounted
+        >
+          {(transitionStyle) => (
+            <form style={transitionStyle}>
+              <Text mb={10} c={"dimmed"} className={classes.subtitle}>
+                Enter OTP
+              </Text>
+              <PinInput
+                onComplete={(values) => handleSubmitPin(values)}
+                autoFocus
+                mask
+                oneTimeCode
+                error={displayAlert}
+              />
+              <Group mt={3} gap={0}>
+                <ThemeIcon
+                  className={`${classes.errorMessage} ${classes.caution}`}
+                  style={{
+                    display: displayAlert ? "flex" : "none",
+                  }}
+                >
+                  <IconAlertTriangle size={20} />
+                </ThemeIcon>
+                <Text
+                  style={{
+                    display: displayAlert ? "block" : "none",
+                  }}
+                  className={classes.errorMessage}
+                  children={alertMessage}
+                ></Text>
+              </Group>
+            </form>
+          )}
+        </Transition>
+      </Flex>
+
+      {/* Reset Password Successful */}
+      <Group style={{ display: displaySuccess ? "block" : "none" }}>
+        <Transition
+          mounted={displaySuccess}
+          transition={"fade-right"}
+          duration={200}
+          timingFunction="ease"
+          keepMounted
+        >
+          {(transitionStyle) => (
+            <Group style={transitionStyle} mt={1} mb={5} gap={0}>
+              <ThemeIcon
+                className={classes.successMessage}
+                style={{
+                  display: displaySuccess ? "flex" : "none",
+                }}
+              >
+                <IconCheck strokeWidth={2.5} size={20} />
+              </ThemeIcon>
+              <Text
+                style={{
+                  display: displaySuccess ? "block" : "none",
+                }}
+                className={classes.successMessage}
+              >
+                Password Reset Successfully
+              </Text>
+            </Group>
+          )}
+        </Transition>
+      </Group>
     </Flex>
   );
 }
