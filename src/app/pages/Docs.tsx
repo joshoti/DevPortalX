2dfbad8174a3f1f428c8a1b021f2a2135dd28cab
joshoti <joshoti539@gmail.com>
2024-10-22 23:52:00 -0700

    Removed unused components from the docsLayout Component

--- src/app/pages/Docs.tsx
+++ src/app/pages/Docs.tsx
@@ -1,18 +1,24 @@
+import { Outlet } from "react-router-dom";
+import { DocsMenu } from "../components/Docs/DocsMenu";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
-import { DocsMenu } from "../components/Docs/DocsMenu";
-import { DocsLogin } from "../components/Docs/DocsLogin";
-import { Flex } from "@mantine/core";
 
-export function DocsTest() {
+export function DocumentationLayout() {
   return (
     <>
       <Header />
-      <Flex>
+      <div style={{ display: "flex" }}>
+        {/* Constant left-side menu */}
         <DocsMenu />
-        <DocsLogin />
-      </Flex>
+
+        {/* Right-side dynamic content */}
+        <div style={{ flex: 1, padding: "20px", position: "sticky", top: 80 }}>
+          <Outlet />
+        </div>
+      </div>
       <Footer />
     </>
   );
 }
+
+export default DocumentationLayout;
