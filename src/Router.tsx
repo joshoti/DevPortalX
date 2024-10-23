0755775f6372816db54fa1f8ffb9808e429d33cd
joshoti <joshoti539@gmail.com>
2024-10-22 23:49:14 -0700

    Routing technique for documentation pages worked

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { DocsTest } from "./app/pages/Docs";
+import { DocumentationLayout } from "./app/pages/Docs";
 import { HomePage } from "./app/pages/Home";
 import { Login } from "./app/pages/Login";
 import { Register } from "./app/pages/Register";
 import { Support } from "./app/pages/Support";
+
+import { DocsOverview } from "./app/components/Docs/DocsOverview";
+import { DocsLogin } from "./app/components/Docs/DocsLogin";
+import { DocsWebhooks } from "./app/components/Docs/DocsWebhooks";
+import { DocsAnalytics } from "./app/components/Docs/DocsAnalytics";
 
 const router = createBrowserRouter([
   {
@@ -19,12 +24,26 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
-    path: "/docs/overview",
-    element: <DocsTest />,
-  },
-  {
-    path: "/docs/login-api",
-    element: <DocsTest />,
+    path: "/docs", // Base path
+    element: <DocumentationLayout />, // Constant layout
+    children: [
+      {
+        path: "overview",
+        element: <DocsOverview />,
+      },
+      {
+        path: "login",
+        element: <DocsLogin />,
+      },
+      {
+        path: "webhooks",
+        element: <DocsWebhooks />,
+      },
+      {
+        path: "analytics",
+        element: <DocsAnalytics />,
+      },
+    ],
   },
   {
     path: "/support",
