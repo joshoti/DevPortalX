a1450f0fceaedd7696d8e99c38bee1cbc2611bc7
joshoti <joshoti539@gmail.com>
2024-10-22 17:38:37 -0700

    done with the support page

--- src/app/components/Support/GetSupport.tsx
+++ src/app/components/Support/GetSupport.tsx
@@ -8,15 +8,25 @@ import {
   Transition,
   Title,
   Textarea,
-  FileInput,
+  Paper,
+  FileButton,
+  CloseButton,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import classes from "./Support.module.css";
-import { IconAt, IconAlertTriangle, IconCheck } from "@tabler/icons-react";
+import {
+  IconAt,
+  IconAlertTriangle,
+  IconXboxX,
+  IconSend,
+} from "@tabler/icons-react";
 import { useState } from "react";
 import { api, fetchCsrfToken } from "../../api/axios-api";
+import { Link } from "react-router-dom";
 
 export function GetSupport() {
+  const [fileAttached, setFileAttached] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
   const [displayForm, setDisplayForm] = useState(true);
   const [displaySuccess, setDisplaySuccess] = useState(false);
   const [displayAlert, setDisplayAlert] = useState(false);
@@ -39,19 +49,44 @@ export function GetSupport() {
   });
 
   // const textInputStyle = { width: 400 };
+  const paperStyle = { radius: 40 };
+  const successIconSize = 70;
+  const uploadButtonStyle = {
+    borderColor: "#5345c8",
+    backgroundColor: "white",
+    color: "#5345c8",
+  };
+  const textInputRadius = "md";
   const inputFieldIconStyle = { size: 18, stroke: 1.5 };
   type SupportT = {
     email: string;
     subject: string;
     message: string;
-    attachment: Blob | null;
+    attachment: File | null;
+  };
+
+  const attachFile = (attachment: File | null) => {
+    if (!attachment) return;
+    setFile(attachment);
+    setFileAttached(true);
+  };
+
+  const removeAttachment = () => {
+    setFile(null);
+    setFileAttached(false);
   };
 
   const handleSubmit = async (form: SupportT) => {
+    form.attachment = file;
     const csrfToken = await fetchCsrfToken();
 
     api
-      .post("/api/support", form, { headers: { "X-CSRF-TOKEN": csrfToken } })
+      .post("/api/support", form, {
+        headers: {
+          "X-CSRF-TOKEN": csrfToken,
+          "Content-Type": "multipart/form-data",
+        },
+      })
       .then(() => {
         setDisplayForm(false);
         setDisplaySuccess(true);
@@ -66,61 +101,111 @@ export function GetSupport() {
   };
 
   return (
-    <Flex align={"center"} justify={"center"} h={"100%"} flex={1}>
+    <Group align={"center"} justify={"center"} h={"100%"}>
       {/* Get Support Form */}
-      <Flex
-        style={{ display: displayForm ? "flex" : "none" }}
-        direction="column"
-      >
+      <Flex direction="column" w={770}>
         <Title mt={30} className={classes.title}>
           Support
         </Title>
-        <Text mb={30} c={"dimmed"} className={classes.subtitle}>
+        <Text mb={30} className={classes.subtitle}>
           Tell us about a problem you'd like to report or feedback you have
           about your experience.
         </Text>
-        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
-          <TextInput
-            leftSection={
-              <IconAt
-                stroke={inputFieldIconStyle.stroke}
-                size={inputFieldIconStyle.size}
+        <form
+          style={{ display: displayForm ? "block" : "none" }}
+          onSubmit={form.onSubmit((values) => handleSubmit(values))}
+        >
+          <Paper
+            radius={paperStyle.radius}
+            className={classes.singlePaperContainer}
+          >
+            <Text className={classes.singlePaperTitle}>
+              Contact Information
+            </Text>
+            <TextInput
+              leftSection={
+                <IconAt
+                  stroke={inputFieldIconStyle.stroke}
+                  size={inputFieldIconStyle.size}
+                />
+              }
+              withAsterisk
+              mb={15}
+              size="md"
+              radius={textInputRadius}
+              label="Email Address"
+              placeholder="your@email.com"
+              key={form.key("Email")}
+              {...form.getInputProps("email")}
+            />
+          </Paper>
+
+          <Paper
+            radius={paperStyle.radius}
+            className={classes.singlePaperContainer}
+          >
+            <Text className={classes.singlePaperTitle}>Feedback</Text>
+            <TextInput
+              withAsterisk
+              mb={15}
+              size="md"
+              radius={textInputRadius}
+              label="Subject"
+              placeholder="Subject"
+              key={form.key("subject")}
+              {...form.getInputProps("subject")}
+            />
+            <Textarea
+              withAsterisk
+              mb={15}
+              size="md"
+              radius={textInputRadius}
+              label="How can we help?"
+              placeholder="Message"
+              key={form.key("message")}
+              {...form.getInputProps("message")}
+            />
+          </Paper>
+
+          <Paper
+            radius={paperStyle.radius}
+            className={classes.singlePaperContainer}
+          >
+            <Text mb={5} className={classes.singlePaperTitle}>
+              Attachments (optional)
+            </Text>
+            <Text className={classes.singlePaperDescription}>
+              You can upload an image or a pdf document
+            </Text>
+
+            <Flex align={"center"}>
+              <FileButton
+                onChange={(value) => attachFile(value)}
+                accept="image/*, .pdf"
+              >
+                {(props) => (
+                  <Button
+                    className={classes.button}
+                    radius={textInputRadius}
+                    style={uploadButtonStyle}
+                    {...props}
+                  >
+                    <Text fz={15} fw={600} m={0} p={0}>
+                      {fileAttached ? file?.name : "Upload"}
+                    </Text>
+                  </Button>
+                )}
+              </FileButton>
+              <CloseButton
+                ml={5}
+                onClick={removeAttachment}
+                display={fileAttached ? "block" : "none"}
+                c={"#5345c8"}
+                variant="transparent"
+                icon={<IconXboxX size={20} stroke={1.5} />}
               />
-            }
-            withAsterisk
-            mb={15}
-            size="md"
-            label="Email"
-            placeholder="your@email.com"
-            key={form.key("Email")}
-            {...form.getInputProps("email")}
-          />
-          <TextInput
-            withAsterisk
-            mb={15}
-            size="md"
-            label="Subject"
-            placeholder="Subject"
-            key={form.key("subject")}
-            {...form.getInputProps("subject")}
-          />
-          <Textarea
-            withAsterisk
-            mb={15}
-            size="md"
-            label="How can we help?"
-            placeholder="Message"
-            key={form.key("message")}
-            {...form.getInputProps("message")}
-          />
-          <FileInput
-            size="md"
-            label="Attachments"
-            description="Optional"
-            placeholder="Drag or Click to upload attachments"
-            variant="filled"
-            // h={300}
-          />
+            </Flex>
+          </Paper>
 
           <Group mt={3} gap={0}>
             <ThemeIcon
@@ -136,49 +221,79 @@ export function GetSupport() {
                 display: displayAlert ? "block" : "none",
               }}
               className={classes.errorMessage}
-              children={alertMessage}
-            ></Text>
+            >
+              {alertMessage}
+            </Text>
           </Group>
 
-          <Group mt="lg" mb="md">
+          <Group mt="lg" mb={50}>
             <Button fullWidth color="#5345c8" type="submit">
               Submit Form
             </Button>
           </Group>
         </form>
-      </Flex>
 
-      {/* Request Submit Successful */}
-      <Group style={{ display: displaySuccess ? "block" : "none" }}>
+        {/* Request Submit Successful */}
         <Transition
           mounted={displaySuccess}
-          transition={"fade-right"}
-          duration={200}
+          transition={"fade-up"}
+          duration={800}
           timingFunction="ease"
           keepMounted
         >
           {(transitionStyle) => (
-            <Group style={transitionStyle} mt={1} mb={5} gap={0}>
-              <ThemeIcon
-                className={classes.successMessage}
-                style={{
-                  display: displaySuccess ? "flex" : "none",
-                }}
-              >
-                <IconCheck strokeWidth={2.5} size={20} />
-              </ThemeIcon>
-              <Text
-                style={{
-                  display: displaySuccess ? "block" : "none",
-                }}
-                className={classes.successMessage}
-              >
-                Request Submitted Successfully
-              </Text>
+            <Group style={{ display: displaySuccess ? "block" : "none" }}>
+              <Paper style={transitionStyle} mt={1} mb={5}>
+                {/* Black header */}
+                <Group
+                  h={140}
+                  justify="center"
+                  style={{ backgroundColor: "black" }}
+                >
+                  <Text mb={5} className={classes.successTitle}>
+                    Thank You!
+                  </Text>
+                </Group>
+
+                {/* Body */}
+                <Group className={classes.singlePaperContainer}>
+                  <Text className={classes.subtitle}>
+                    Request submitted successfully. Kindly give at most 4
+                    business days to get a response to your feedback.
+                  </Text>
+                  <Group w={"100%"} mt={30} justify="space-around">
+                    <ThemeIcon
+                      size={successIconSize}
+                      className={classes.successMessage}
+                    >
+                      <IconSend strokeWidth={2} size={successIconSize} />
+                    </ThemeIcon>
+                    <ThemeIcon
+                      size={successIconSize}
+                      className={classes.successMessage}
+                    >
+                      <IconSend strokeWidth={2} size={successIconSize} />
+                    </ThemeIcon>
+                    <ThemeIcon
+                      size={successIconSize}
+                      className={classes.successMessage}
+                    >
+                      <IconSend strokeWidth={2} size={successIconSize} />
+                    </ThemeIcon>
+                  </Group>
+                </Group>
+              </Paper>
+
+              {/* Redirect Button */}
+              <Link to="/">
+                <Button mt="lg" mb={50} fullWidth color="#5345c8">
+                  Go To Homepage
+                </Button>
+              </Link>
             </Group>
           )}
         </Transition>
-      </Group>
-    </Flex>
+      </Flex>
+    </Group>
   );
 }
