46f37fcd71b696d59e6c09fd5776f7d1f428ae1f
joshoti <joshoti539@gmail.com>
2024-10-25 10:09:07 -0700

    made tableOfContent for docs Overview page

--- src/app/components/Docs/DocsOverview.tsx
+++ src/app/components/Docs/DocsOverview.tsx
@@ -1,105 +1,48 @@
-import {
-  Button,
-  Group,
-  TextInput,
-  Text,
-  Flex,
-  Title,
-  Textarea,
-  Paper,
-} from "@mantine/core";
+import { Text, Flex, Title } from "@mantine/core";
 import classes from "./Docs.module.css";
-import { IconAt } from "@tabler/icons-react";
 import { useEffect } from "react";
-import { scrollBehavior } from "../../pages/Docs";
+import { docsScrollOptions } from "../../pages/Docs";
+import { scrollToElement } from "../../utils/scoll";
 
 export function DocsOverview() {
   // To scroll to the top manually
   useEffect(() => {
-    const head = document.getElementById("head");
-    head?.scrollIntoView(scrollBehavior);
+    scrollToElement("head", docsScrollOptions);
   });
 
-  const paperStyle = { radius: 40 };
-  const textInputRadius = "md";
-  const inputFieldIconStyle = { size: 18, stroke: 1.5 };
+  const marginTop = 40;
 
   return (
     <Flex h={"100%"}>
-      {/* Documentation page */}
+      {/* Content section */}
       <Flex flex={1} direction="column">
-        <Title id="head" mt={30} className={classes.title}>
-          Overview
+        <Title
+          id="head"
+          mt={marginTop}
+          mb={marginTop / 2}
+          className={classes.title}
+        >
+          Overview of Documentation
         </Title>
-        <Text mb={30} className={classes.subtitle}>
-          Tell us about a problem you'd like to report or feedback you have
-          about your experience.
+        <Text mb={30} className={classes.regularText}>
+          Welcome to the Developers documentation. Here we'll show you how to
+          set up your Developer account and start integrating your app with our
+          APIs.
         </Text>
-        <Flex direction="column">
-          <Paper
-            radius={paperStyle.radius}
-            className={classes.singlePaperContainer}
-          >
-            <Text className={classes.singlePaperTitle}>
-              Contact Information
-            </Text>
-            <TextInput
-              leftSection={
-                <IconAt
-                  stroke={inputFieldIconStyle.stroke}
-                  size={inputFieldIconStyle.size}
-                />
-              }
-              withAsterisk
-              mb={15}
-              size="md"
-              radius={textInputRadius}
-              label="Email Address"
-              placeholder="your@email.com"
-            />
-          </Paper>
+      </Flex>
 
-          <Paper
-            radius={paperStyle.radius}
-            className={classes.singlePaperContainer}
-          >
-            <Text className={classes.singlePaperTitle}>Feedback</Text>
-            <TextInput
-              withAsterisk
-              mb={15}
-              size="md"
-              radius={textInputRadius}
-              label="Subject"
-              placeholder="Subject"
-            />
-            <Textarea
-              withAsterisk
-              mb={15}
-              size="md"
-              radius={textInputRadius}
-              label="How can we help?"
-              placeholder="Message"
-            />
-          </Paper>
-
-          <Paper
-            radius={paperStyle.radius}
-            className={classes.singlePaperContainer}
-          >
-            <Text mb={5} className={classes.singlePaperTitle}>
-              Attachments (optional)
-            </Text>
-            <Text className={classes.singlePaperDescription}>
-              You can upload an image or a pdf document
-            </Text>
-          </Paper>
-
-          <Group mt="lg" mb={50}>
-            <Button fullWidth color="#5345c8" type="submit">
-              Submit Form
-            </Button>
-          </Group>
-        </Flex>
+      {/* Table of Contents */}
+      <Flex flex={0.4} direction="column" className={classes.tocContainer}>
+        <Text
+          className={classes.tableOfContentTitle}
+          mt={marginTop + 10}
+          mb={marginTop / 2}
+        >
+          Table of Contents
+        </Text>
+        <Text className={classes.tableOfContentText}>Login API</Text>
+        <Text className={classes.tableOfContentText}>Analytics</Text>
+        <Text className={classes.tableOfContentText}>Webhooks</Text>
       </Flex>
     </Flex>
   );
