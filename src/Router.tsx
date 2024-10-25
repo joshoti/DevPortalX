5517a49ad62cca753355a83abf072f188f7fd6a7
joshoti <joshoti539@gmail.com>
2024-10-24 20:57:43 -0700

    Changed router implementation so ScrollToTop can be used

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
 import { DocumentationLayout } from "./app/pages/Docs";
 import { HomePage } from "./app/pages/Home";
 import { Login } from "./app/pages/Login";
@@ -10,47 +12,33 @@ import { DocsLogin } from "./app/components/Docs/DocsLogin";
 import { DocsWebhooks } from "./app/components/Docs/DocsWebhooks";
 import { DocsAnalytics } from "./app/components/Docs/DocsAnalytics";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/docs", // Base path
-    element: <DocumentationLayout />, // Constant layout
-    children: [
-      {
-        path: "overview",
-        element: <DocsOverview />,
-      },
-      {
-        path: "login",
-        element: <DocsLogin />,
-      },
-      {
-        path: "webhooks",
-        element: <DocsWebhooks />,
-      },
-      {
-        path: "analytics",
-        element: <DocsAnalytics />,
-      },
-    ],
-  },
-  {
-    path: "/support",
-    element: <Support />,
-  },
-]);
-
 export function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/support" element={<Support />} />
+
+        <Route path="/docs" element={<DocumentationLayout />}>
+          <Route path="overview" element={<DocsOverview />} />
+          <Route path="login" element={<DocsLogin />} />
+          <Route path="webhooks" element={<DocsWebhooks />} />
+          <Route path="analytics" element={<DocsAnalytics />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
