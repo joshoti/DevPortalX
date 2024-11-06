de6ea7bb8ad8a22787e3117769c4af11b200b2ea
joshoti <joshoti539@gmail.com>
2024-11-05 17:52:42 -0800

    Completed the footer properly

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,117 +1,97 @@
 import {
-  IconDeviceLaptop,
   IconCopyright,
-  IconBrandInstagram,
   IconBrandGithubFilled,
   IconBrandLinkedin,
 } from "@tabler/icons-react";
-import {
-  Group,
-  Text,
-  Container,
-  Tabs,
-  rem,
-  Button,
-  Anchor,
-  Image,
-  Divider,
-} from "@mantine/core";
+import { Group, Text, Anchor, Image, Divider, Flex, Box } from "@mantine/core";
 import IgLogo from "../../assets/images/igLogo.jpg";
 import classes from "./Footer.module.css";
+import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
-  const iconStyle = { width: rem(15), height: rem(15) };
-  const iconStylePanel = { width: rem(20), height: rem(20) };
+  const iconSize = 45;
+  const siteNameSize = 25;
+  const navigate = useNavigate();
+
+  function goToPage(link: string) {
+    navigate(link, { replace: true });
+  }
 
   return (
-    <Container h={150} className={classes.container}>
-      <Divider pb={20} my={0} />
-      <Group>
-        <Group gap={4}>
-          <IconCopyright size={18} color="gray" />
-          <Text
-            fw={400}
-            size="lg"
-            style={{ color: "gray", margin: 0, padding: 0 }}
-          >
-            2024
-          </Text>
-          <Text className={`${classes.title}`}>DevPortalX</Text>
-          <IconDeviceLaptop size={25} color="white" />
-        </Group>
-
-        <Group className={`${classes.headerRight}`}>
-          <Tabs defaultValue="connect">
-            <Tabs.List>
-              <Tabs.Tab
-                value="connect"
-                className={classes.tabs}
-                leftSection={
-                  <IconBrandLinkedin color="white" style={iconStyle} />
-                }
-              >
-                <Text className={classes.text}>Connect</Text>
-              </Tabs.Tab>
-              <Tabs.Tab
-                value="with"
-                className={classes.tabs}
-                leftSection={
-                  <IconBrandGithubFilled color="white" style={iconStyle} />
-                }
-              >
-                <Text className={classes.text}>With</Text>
-              </Tabs.Tab>
-              <Tabs.Tab
-                value="me"
-                className={classes.tabs}
-                leftSection={
-                  <IconBrandInstagram color="white" style={iconStyle} />
-                }
-              >
-                <Text className={classes.text}>Me</Text>
-              </Tabs.Tab>
-            </Tabs.List>
-            <Tabs.Panel value="connect">
-              <Anchor
-                href="https://www.linkedin.com/in/joshua-oti/"
-                target="_blank"
-              >
-                <Button
-                  variant="transparent"
-                  leftSection={<IconBrandLinkedin style={iconStylePanel} />}
-                >
-                  <Text className={`${classes.text}`}>joshua-oti</Text>
-                </Button>
-              </Anchor>
-            </Tabs.Panel>
-            <Tabs.Panel value="with">
-              <Anchor href="https://github.com/joshoti" target="_blank">
-                <Button
-                  variant="transparent"
-                  leftSection={
-                    <IconBrandGithubFilled
-                      color="white"
-                      style={iconStylePanel}
-                    />
-                  }
-                >
-                  <Text className={classes.text}>joshoti</Text>
-                </Button>
-              </Anchor>
-            </Tabs.Panel>
-            <Tabs.Panel value="me">
-              <Anchor href="https://instagram.com/josh_ohtee" target="_blank">
-                <Button
-                  variant="transparent"
-                  leftSection={<Image src={IgLogo} h={23} />}
-                >
-                  <Text className={classes.text}>josh_ohtee</Text>
-                </Button>
-              </Anchor>
-            </Tabs.Panel>
-          </Tabs>
-        </Group>
+    <Box className={classes.container}>
+      <Divider pb={20} my={0} w={"100%"} />
+      <Group gap={4} className={classes.siteName}>
+        <IconCopyright size={siteNameSize} color="gray" />
+        <Text
+          fw={400}
+          fz={siteNameSize}
+          style={{ color: "gray", margin: 0, padding: 0 }}
+        >
+          2024
+        </Text>
+        <Text className={classes.title}>DevPortalX</Text>
       </Group>
-    </Container>
+
+      <Flex justify="space-between" className={classes.footerContent}>
+        {/* Products */}
+        <Flex direction="column">
+          <Text className={classes.subtitle}>Products</Text>
+          <Text
+            className={classes.clickable}
+            onClick={() => goToPage("/docs/login")}
+          >
+            OAuth 2.0 API
+          </Text>
+          <Text
+            className={classes.clickable}
+            onClick={() => goToPage("/docs/webhooks")}
+          >
+            Webhooks API
+          </Text>
+          <Text
+            className={classes.clickable}
+            onClick={() => goToPage("/docs/analytics")}
+          >
+            Analytics API
+          </Text>
+        </Flex>
+
+        {/* Support */}
+        <Flex direction="column">
+          <Text className={classes.subtitle}>Support</Text>
+          <Text
+            className={classes.clickable}
+            onClick={() => goToPage("/support")}
+          >
+            Get Support
+          </Text>
+          <Text className={classes.text}>Contribute</Text>
+        </Flex>
+
+        {/* Company */}
+        <Flex direction="column">
+          <Text className={classes.subtitle}>Company</Text>
+          <Text className={classes.text}>About Us</Text>
+          <Text className={classes.text}>Events</Text>
+          <Text className={classes.text}>Contact</Text>
+        </Flex>
+
+        {/* Contact */}
+        <Flex direction="column" gap={10}>
+          <Anchor
+            href="https://www.linkedin.com/in/joshua-oti/"
+            target="_blank"
+          >
+            <IconBrandLinkedin size={iconSize} />
+          </Anchor>
+          <Anchor href="https://github.com/joshoti" target="_blank">
+            <IconBrandGithubFilled color="white" size={iconSize} />
+          </Anchor>
+          <Anchor href="https://instagram.com/josh_ohtee" target="_blank">
+            <Image src={IgLogo} w={iconSize} h={iconSize} />
+          </Anchor>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
